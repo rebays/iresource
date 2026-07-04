2bd4c7808b8be5ad5723b41d530c43fb266e80fb
thisisjoshu <joshwhizkid@gmail.com>
2026-07-05 10:57:26 +1100

    initial commit

---
+++ cms/iresource/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mf25heny$ldw_r7te-5n6=i0-&nds7@(xyw5ji3-(lhnz01%7y"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
